cf3935aed01edc1509d7f20b37b94d7958b1bf19
FIC <alexandre.adami@uol.com.br>
2017-10-19 21:35:41 -0200

    19/10 21:35

--- Program.cs
+++ Program.cs
@@ -8,12 +8,17 @@ namespace sistema_vendas
         {
             string opcao = "";
             do{
+                
+                Console.WriteLine("==========================");
+                Console.WriteLine();
                 Console.WriteLine("Digite a opção:");
                 Console.WriteLine("1 - Cadastrar Cliente");
                 Console.WriteLine("2 - Cadastrar Produto");
                 Console.WriteLine("3 - Realizar Venda");
                 Console.WriteLine("4 - Extrato Cliente");
                 Console.WriteLine("9 - Sair");
+                Console.WriteLine();
+                Console.WriteLine("===========================");
 
                 //Recebe opção do Cliente
                 opcao = Console.ReadLine();
@@ -58,6 +63,27 @@ namespace sistema_vendas
         static void ExtratoCliente(){
 
         }
+
+        //metodo para validar o cpf
+        static bool ValidaCpf(string cpf){
+            bool cpfvalido = false;
+
+            if(cpf == "55349862120")
+                cpfvalido = true;
+
+                return cpfvalido;
+        }
+
+        //metodo valida cnpj
+        static bool ValindaCnpj(string cnpj){
+            bool cnpjvalido = false;
+
+            if(cnpj == "00744472000159")
+                cnpjvalido = true;
+                return cnpjvalido;
+            
+        }
+
 
     }
 }
